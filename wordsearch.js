197bfdc893976a78e4c3678d3fe598c5b5ee6f3f
Chibu <atuchibueze@gmail.com>
2022-05-03 09:40:57 -0400

    implement wordsearch & fix bug

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (!word) {
+        return false;
     }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (let l of horizontalJoin) {
+        // if (l.includes(word)) return true
+        if (l === word) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
